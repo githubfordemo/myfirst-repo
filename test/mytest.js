c2a952e8fd193853a0a02bf3d8ab59ca45d870c7
unknown <Ishwer.bhattarai@ISHWERP-01.no.org.software-innovation.com>
2015-04-14 18:19:46 +0530

    fist

---
+++ test/mytest.js
@@ -0,0 +1,9 @@
+﻿var expect = require("chai").expect;
+
+describe("demo test", function () {
+    it("should be passed", function () {
+        var arr = [3, 3, 4]
+        expect(arr.length).to.be.equal(3);
+        expect(arr.length).to.be.at.least(1);
+    });
+});
